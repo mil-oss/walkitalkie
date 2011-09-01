d09004641b74b92e085b83b8e283b02a45c7f96b
Patrick Reynolds <cpreynolds@gmail.com>
2011-09-01 13:06:48 -0400

    This crashes now, but in theory it should do basic record and playback.

--- src/org/miloss/TalkActivity.java
+++ src/org/miloss/TalkActivity.java
@@ -1,13 +1,27 @@
 package org.miloss;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.MediaStore.Audio;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioRecord.OnRecordPositionUpdateListener;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
 
 public class TalkActivity extends Activity {
   
@@ -16,6 +30,12 @@ public class TalkActivity extends Activity {
      */
     Button mTalkButton;
     Button mGuysButton;
+    
+    /**
+     * Media player and recorder
+     */
+    AudioRecord mRecorder;
+    ByteBuffer  mAudioBuffer;
     
     public static final String TAG = "TalkActivity";
     
@@ -29,9 +49,30 @@ public class TalkActivity extends Activity {
         switch (motion.getAction()) {
         case MotionEvent.ACTION_DOWN:
           Log.d(TAG, "Talk Down");
+          try {
+            startRecording();
+          } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
           break;
         case MotionEvent.ACTION_UP:
           Log.d(TAG, "Talk Up");
+          try {
+            stopRecording();
+          } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
           break;
         }
         
@@ -47,6 +88,28 @@ public class TalkActivity extends Activity {
         Log.d(TAG, "Guys Click");
       }
     };
+    
+    private void startRecording() throws IllegalStateException, IOException {
+     MediaRecorder recorder = new MediaRecorder();
+     recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+     recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+     recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+     FileOutputStream file = new FileOutputStream("/sdcard/output.sound");
+     recorder.setOutputFile(file.getFD());
+     recorder.prepare();
+     recorder.start(); 
+  }
+
+  private void stopRecording() throws IllegalArgumentException, IllegalStateException, IOException {
+      mRecorder.stop();
+      mRecorder.release();
+      mRecorder = null;
+      MediaPlayer mediaPlayer = new MediaPlayer();
+      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+      mediaPlayer.setDataSource("/sdcard/output.sound");
+      mediaPlayer.prepare(); // might take long! (for buffering, etc)
+      mediaPlayer.start();
+  }
     
     /** Get the refs to the ui elements on the page */
     public void setupUIReferences() {
@@ -65,6 +128,7 @@ public class TalkActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
         setupUIReferences();
         setupEventListeners();
